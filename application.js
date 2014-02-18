f92bcadc62c1a7191709d4eb3634f1432c1a93bb
Alex <alexv@new-host.home>
2014-02-17 19:03:00 -0500

    A

--- application.js
+++ application.js
@@ -1,14 +1,54 @@
+// A simple (it does the job) function for template parsing.
+ $(document).ready(function() {
+            $('.fancybox').fancybox();
+        });
+
 var Instagram = {};
+Instagram.Template = {};
+
+Instagram.Template.Views = {
+
+  "photo": "<div class='photo'>" +
+            "<a href='{instagram.images.thumbnail.url}' class='fancybox'><img class='main' src='{photo_url}' width='250' height='250' /></a>" +
+            "<img class='avatar_url' width='40' height='40' src='{avatar}' />" +
+            "<span class='heart'><strong>{like_count}</strong></span>" +
+          "</div>"
+};
+
+function toTemplate(photo){
+	photo = {
+		like_count: photo.likes.count,
+		avatar_url: photo.user.profile_picture,
+		photo_url: photo.images.low_resolution.url,
+		url:photo.link
+
+
+};
+
+  function paginate(max_id){    
+    $.getJSON(generateUrl(tag), toScreen);
+  }
+
+return Instagram.Template.generate('photo', Instagram.Template.View['photo']);
+}
 
 (function(){
 
  function toScreen(photos){
-  $.each(photos.data, function(index,photo){
-  	photo = "<img src='" + photo.images.low_resolution.url + "' />";
-  	$('div#photos-wrap').append(photo);
-}); 
-
-
+ 	var photos_html = '';
+    $('.paginate a').attr('data-max-tag-id', photos.pagination.next_max_id)
+                    .fadeIn();
+	  $.each(photos.data, function(index, photo){
+  
+  	photo = "<div class='photo'>" +
+    "<a href='"+ photo.images.low_resolution.url + "' class='fancybox' rel='gallery'>"+
+      "<img class='main' src='" + photo.images.thumbnail.url + "' width='250' height='250' />" +
+    "</a>" +
+    "<img class='avatar' width='40' height='40' src='"+photo.user.profile_picture+"' />" +
+    "<span class='heart'><strong>"+photo.likes.count+"</strong></span>" +
+  "</div>";
+  $('div#photos-wrap').append(photo);
+});
   }
 
 function search(tag){
